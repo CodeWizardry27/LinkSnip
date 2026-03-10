835a2c85e679f22673ff9d79bdb3013184bdbf9c
CodeWizardry27 <satyamjha88anand@gmail.com>
2026-03-10 15:22:00 +0530

    init spring boot project with dependencies

---
+++ src/main/java/com/linksnip/LinkSnipApplication.java
@@ -0,0 +1,12 @@
+package com.linksnip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSnipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkSnipApplication.class, args);
+    }
+}
